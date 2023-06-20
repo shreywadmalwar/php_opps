a8c76b2faa0866d5b38a55f9690421c0b903d16b
Shreyash Devendra Wadmalwar <sdwadmalwar@gmail.com>
2023-06-20 10:27:02 +0530

    Add files via upload
    This file contains code related to encapsulation in PHP.

---
+++ encapsulation.php
@@ -0,0 +1,22 @@
+<?php 
+
+class LightSwitch {
+
+    public function on() {
+
+    }
+    public function off() {
+
+
+    }
+
+    private function connect() {
+
+    }
+
+    private function activate() {
+
+    }
+}
+?>
+
